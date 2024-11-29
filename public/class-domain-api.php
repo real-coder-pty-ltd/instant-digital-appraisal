5153fbad7bc0a1f865b9cbc7193c57535c7e7675
Matthew Neal <matt.neal@stafflink.com.au>
2024-11-29 16:24:17 +1000

    added caching to api getter.

--- public/class-domain-api.php
+++ public/class-domain-api.php
@@ -19,19 +19,26 @@ class Domain_API
 
     public $data;
 
+    public $post_id;
+
+    public $updated;
+
+    public $updated_date;
+
+    public $result;
+
     public function __construct()
     {
 
         $this->api_url = 'https://api.domain.com.au/v2/';
-        $this->api_key = '';
+        $this->api_key = get_option('domain_api_key');
         $this->headers = [
             'accept' => 'application/json',
             'X-Api-Key' => $this->api_key,
         ];
-
     }
 
-    public function get($endpoint, $params, $route = [], $version = 'v2'): array|string
+    public function get($endpoint, $params, $route = [], $version = 'v2')
     {
         $this->endpoint = $this->api_url.$endpoint;
 
@@ -48,6 +55,16 @@ class Domain_API
 
         $this->request_url = add_query_arg($this->query_params, $this->endpoint);
 
+        
+        $data = $this->cacheResult();
+
+        // If we have the cached data, return it.
+        if ($data->updated == 'cached data') {
+            $this->data = $data->data;
+            $this->result = 'True, with cached data.';
+            return $this;
+        }
+
         $response = wp_remote_get($this->request_url, [
             'headers' => $this->headers,
         ]);
@@ -55,15 +72,52 @@ class Domain_API
         if (is_wp_error($response)) {
             $error_message = $response->get_error_message();
             $this->data = 'something went wrong: '.$error_message;
-
-            return false;
+            $this->result = false;
         } else {
             $body = wp_remote_retrieve_body($response);
             $this->data = json_decode($body, true);
-
-            return true;
+            $this->result = true;
         }
+
+        return $this;
+    }
+
+    public function cacheResult() {
+
+        global $post;
+        $this->post_id = $post->ID;
+
+        $cache_key = 'domain_api_data_'.md5($this->request_url);
+        $cache_key_timestamp = 'domain_api_data_timestamp_'.md5($this->request_url);
+        $now = time();
+
+        $cached_data = get_post_meta($this->post_id, $cache_key, true);
+        $timestamp = get_post_meta($this->post_id, $cache_key_timestamp, true);
+
+        if (empty($cached_data) || empty($timestamp)) {
+            update_post_meta($this->post_id, $cache_key, $this->data);
+            update_post_meta($this->post_id, $cache_key_timestamp, time());
+
+            $this->updated = 'new';
+            $this->updated_date = $now;
+
+            return $this;
+        }
+
+        // If the cache is older than 60 days, update the cache.
+        if ( $now - $timestamp > 5184000 ) {
+            update_post_meta($this->post_id, $cache_key, $this->data);
+            update_post_meta($this->post_id, $cache_key_timestamp, time());
+
+            $this->updated = 'updated';
+            $this->updated_date = $now;
+            return $this;
+        }
+
+        $this->data = $cached_data;
+        $this->updated = 'cached data';
+        $this->updated_date = $timestamp;
+
+        return $this;
     }
 }
-
-new Domain_API;
