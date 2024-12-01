e8f2f738aba59fc6be5d5db804b7c7b53a98418b
Matthew Neal <matt.neal@stafflink.com.au>
2024-12-02 08:18:00 +1000

    version bump

--- domain-suburb-profiles.php
+++ domain-suburb-profiles.php
@@ -4,7 +4,7 @@
  * Plugin Name:       Domain Suburb Profiles
  * Plugin URI:        https://realcoder.com.au
  * Description:       Create Suburb Profiles from Domain API.
- * Version:           0.1.0
+ * Version:           0.1.01
  * Author:            Matthew Neal
  * Author URI:        https://realcoder.com.au
  * License:           GPL-2.0+
@@ -18,7 +18,7 @@ if (! defined('WPINC')) {
     exit;
 }
 
-define('RC_IDA_VERSION', '0.1.0');
+define('RC_IDA_VERSION', '0.1.01');
 
 function activate_domain_suburb_profiles()
 {
