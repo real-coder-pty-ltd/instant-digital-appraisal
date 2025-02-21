3bfc33eb2cf6747828e74a9dbec1de0dceda098e
Julius Genetia <153688848+julius-genetia-rc@users.noreply.github.com>
2025-02-21 11:23:34 +0800

    Fixed errors appearing due to issue with distance matrix.

--- includes/class-distance-calculator.php
+++ includes/class-distance-calculator.php
@@ -10,6 +10,7 @@ class DistanceCalculator
 
     public function __construct()
     {
+        $this->data = [];
         $this->apiKey = get_option('dsp_google_maps_server_side_api_key');
 
         if ( ! $this->apiKey) {
