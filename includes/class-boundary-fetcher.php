5758c2d564ddfd77c1a966b0e9139059de4d50d0
Julius Genetia <153688848+julius-genetia-rc@users.noreply.github.com>
2025-02-21 12:15:23 +0800

    Updates on boundary fetcher class.

--- includes/class-boundary-fetcher.php
+++ includes/class-boundary-fetcher.php
@@ -41,29 +41,29 @@ class Boundary_Fetcher
 
     public function getLat()
     {
-        if ($this->data[0]['bounds']['minlat'] === null || $this->data[0]['bounds']['maxlat'] === null) {
+        if (empty($this->data) || !isset($this->data[0]['bounds']['minlat']) || !isset($this->data[0]['bounds']['maxlat'])) {
             return null;
         }
-
+    
         $lat = ($this->data[0]['bounds']['minlat'] + $this->data[0]['bounds']['maxlat']) / 2;
         if ($lat) {
             return $lat;
         }
-
+    
         return null;
     }
-
+    
     public function getLong()
     {
-        if ($this->data[0]['bounds']['minlon'] === null || $this->data[0]['bounds']['maxlon'] === null) {
+        if (empty($this->data) || !isset($this->data[0]['bounds']['minlon']) || !isset($this->data[0]['bounds']['maxlon'])) {
             return null;
         }
-
+    
         $long = ($this->data[0]['bounds']['minlon'] + $this->data[0]['bounds']['maxlon']) / 2;
         if ($long) {
             return $long;
         }
-
+    
         return null;
     }
 
